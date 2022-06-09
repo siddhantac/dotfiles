5e8355683b799be954d05f63c3d15ff0e469d6a9
Siddhanta <siddhanta.c@foodpanda.com>
2022-06-09 10:16:53 +0800

    Improve HOME env var

--- nvim/config.lua
+++ nvim/config.lua
@@ -1,15 +1,17 @@
+HOME = os.getenv("HOME")
+
 -- Custom settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.undofile = true -- maintain undo history between sessions
-vim.opt.undodir = os.getenv("HOME") .. '/.lvim/undodir'
+vim.opt.undodir = HOME .. '/.lvim/undodir'
 vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 5
 vim.opt.expandtab = true
-vim.opt.tabstop = 4 -- show existing tab with 4 spaces width
-vim.opt.shiftwidth = 4 -- when indenting with '>', use 4 spaces width
+vim.opt.tabstop = 4     -- show existing tab with 4 spaces width
+vim.opt.shiftwidth = 4  -- when indenting with '>', use 4 spaces width
 vim.opt.softtabstop = 4 -- control <tab> and <bs> keys to match tabstop
 vim.opt.numberwidth = 5
 -- vim.opt.cursorline = true
