9a6173e72109c55ec4754cafc3338d0f642ca4c6
Siddhanta <siddhanta.c@gmail.com>
2022-06-26 18:24:57 +0800

    Added autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
+-- TODO
+-- Pending plugins
+--     git, file browser, vim test, vim-go, terminal (?), snippets (check nvim-cmp, cmp_luasnip)
+--
 -- Plugins
 -- Run the following command to install packer
 --   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -7,6 +11,10 @@ require('packer').startup(function()
     use 'shaunsingh/nord.nvim'
 
     use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
 
     use {
       'nvim-telescope/telescope.nvim',
@@ -23,8 +31,6 @@ require('packer').startup(function()
         }
       end
     }
-    -- Pending plugins
-    --     which-key, git, file browser, vim test, vim-go, lsp, terminal (?)
 end)
 
 HOME = os.getenv("HOME")
@@ -108,7 +114,65 @@ vim.keymap.set('', '<s-RIGHT>', ':vertical resize -5 <CR>')
 vim.keymap.set('', '<s-UP>', ':resize +5 <CR>')
 vim.keymap.set('', '<s-DOWN>', ':resize -5 <CR>')
 
-require('lspconfig').gopls.setup{}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'gopls' } -- 'clangd', 'rust_analyzer', 'pyright', 'tsserver' 
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
 require ('which-key').register(
   {
       f = {
