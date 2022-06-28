b67177579c982d1804bd335b1808a0c13ef05b61
Siddhanta <siddhanta.c@foodpanda.com>
2022-06-28 12:17:31 +0800

    Move plugin settings into dedicated files. Use floating window for packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,54 +1,3 @@
--- TODO
--- Pending plugins
---     vim test, vim-go, terminal (?), snippets (check nvim-cmp, cmp_luasnip)
---
--- Plugins
--- Run the following command to install packer
---   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'shaunsingh/nord.nvim'
-
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
-    use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup { }
-      end
-    }
-
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-
-    use 'tpope/vim-fugitive'
-
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons', -- optional, for file icons
-      }
-    }
-
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-   }
-end)
 
 require('my_autocmds')
 
@@ -104,6 +53,64 @@ vim.keymap.set('', '<s-UP>', ':resize +5 <CR>')
 vim.keymap.set('', '<s-DOWN>', ':resize -5 <CR>')
 
 
+-- TODO
+-- packer in floating window
+-- Pending plugins
+--     vim test, vim-go, terminal (?), snippets (check nvim-cmp, cmp_luasnip)
+--
+-- Plugins
+-- Run the following command to install packer
+--   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+require('packer').startup({function()
+    use 'wbthomason/packer.nvim'
+    use 'shaunsingh/nord.nvim'
+
+    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup { }
+      end
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    use 'tpope/vim-fugitive'
+
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons', -- optional, for file icons
+      }
+    }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+   }
+end,
+config = {
+  display = {
+    open_fn = require('packer.util').float,
+  }
+}})
+
 -- lspconfig setup
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -122,94 +129,8 @@ end
 -- luasnip setup
 local luasnip = require 'luasnip'
 
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
--- commenter setup
-require('Comment').setup{
-toggler = {
-        ---Line-comment toggle keymap
-        line = '<leader>cc',
-        ---Block-comment toggle keymap
-        block = 'gbc',
-    }
-}
-
--- treesitter setup
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "go" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
--- file explorer setup
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-
-require('plugins')
+require('plugins.nvim-cmp')
+require('plugins.comment')
+require('plugins.treesitter')
+require('plugins.nvim-tree')
+require('plugins.which-key')
---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+-- commenter setup
+require('Comment').setup{
+toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>cc',
+        ---Block-comment toggle keymap
+        block = 'gbc',
+    }
+}
+
