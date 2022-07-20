50c0810e4447dc0df5600cf87323ffe27eaa61c5
Siddhanta <siddhanta.c@foodpanda.com>
2022-07-20 14:37:54 +0800

    Added running of gofmt & goimports on save

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,5 +1,5 @@
 local au = require('au')
--- for more examples, see: 
+-- for more examples, see:
 --    https://gist.github.com/numToStr/1ab83dd2e919de9235f9f774ef8076da
 
 -- au.group('PackerGroup', {
@@ -33,3 +33,35 @@ au.group('NumberToggle', {
 
 au.ColorScheme = { 'nord', 'highlight Normal guibg=#192029'}
 au.VimLeave = { '*', '<cmd>SessionManager save_session<cr>'}
+
+-- following example solution from github issue:
+--   https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+function goimports(wait_ms)
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+
+au.group('go_lsp', {
+    {
+        'BufWritePre',
+        '*.go',
+        vim.lsp.buf.formatting,
+    },
+    {
+        'BufWritePre',
+        '*.go',
+        function()
+            goimports(1000)
+        end
+    }
+})
