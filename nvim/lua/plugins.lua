5b9f96a586d1621de80d8deeab44499134df7e7e
Siddhanta <siddhanta.c@foodpanda.com>
2022-11-30 16:09:13 +0800

    Autocmd to run Packer Sync when plugins are added or removed

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,6 +8,28 @@
 -- Run the following command to install packer
 --   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+-- auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 require('packer').startup({function()
     -- tools and utilities
     use 'wbthomason/packer.nvim'
