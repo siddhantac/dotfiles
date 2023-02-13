97557a15598f2d5520198fc08b2596aa4270ad40
Siddhanta <siddhanta.c@foodpanda.com>
2023-02-13 15:42:34 +0800

    Added espanso config to fetch pr titles

---
+++ scripts/get_pr_title.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+token=$(grep -o 'GITHUB_TOKEN.*' ~/.aliases.local | sed -E "s/.*=(.*)/\\1/")
+echo $(GITHUB_TOKEN=${token} gh pr view https://github.com/deliveryhero/pd-pablo-payment-gateway/pull/$1 --json title --jq '.title')
