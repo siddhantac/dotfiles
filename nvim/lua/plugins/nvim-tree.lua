e678c763f05126a56929c729dbbe6bee5ce310d5
Siddhanta <siddhanta.c@foodpanda.com>
2023-02-14 11:26:35 +0800

    Removed deprecated options from nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,8 +1,6 @@
 -- file explorer setup
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
-  open_on_setup = true,
-  open_on_setup_file = true,
   open_on_tab = true,
   update_focused_file = {
       enable = true,
