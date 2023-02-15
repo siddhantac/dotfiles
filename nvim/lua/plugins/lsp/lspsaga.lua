8ec6c9cfef3d46f2798ab4e86ae792ebfdbfdded
Siddhanta <siddhanta.c@foodpanda.com>
2023-02-15 17:10:55 +0800

    Fix lspsaga keybinds

--- nvim/lua/plugins/lsp/lspsaga.lua
+++ nvim/lua/plugins/lsp/lspsaga.lua
@@ -8,16 +8,24 @@ saga.setup({
   -- keybinds for navigation in lspsaga window
   move_in_saga = { prev = "<C-k>", next = "<C-j>" },
   -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
+  finder = {
+      keys = {
+        open = "<CR>",
+        vsplit = '<C-v>',
+        split = '<C-h>',
+        tabe = '<C-t>',
+        quit = {'q', '<ESC>'},
+        close_in_preview = "<Esc>",
+      }
   },
   -- use enter to open file with definition preview
-  definition_action_keys = {
+  definition = {
     edit = "<CR>",
     vsplit = '<C-v>',
     split = '<C-h>',
     tabe = '<C-t>',
     quit = 'q',
+    close = "<Esc>",
   },
 })
 
