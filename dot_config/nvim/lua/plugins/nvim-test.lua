ecbb47fbac5afdb771c6087218426c0b21353fae
Siddhanta <siddhanta.c@gmail.com>
2023-04-20 16:55:45 +0800

    [nvim] add test commands to which-key

--- dot_config/nvim/lua/plugins/nvim-test.lua
+++ dot_config/nvim/lua/plugins/nvim-test.lua
@@ -12,3 +12,18 @@ require('nvim-test.runners.go-test'):setup {
         DH_SPEC_FILE = '/Users/s.c/workspace/deliveryhero/pd-pablo-payment-gateway/deployments/development/spec_local.yaml',
     },
 }
+
+require ('which-key').register(
+  {
+      t = {
+          name = "Test",
+          n = {"<cmd>TestNearest AWS_SECRET_ACCESS_KEY=123 AWS_ACCESS_KEY_ID=123 DH_SPEC_FILE=$(pwd)/deployments/development/spec_local.yaml<cr>", "run test nearest to cursor"},
+          f = {"<cmd>TestFile<cr>", "run test file"},
+          s = {"<cmd>TestSuite<cr>", "run entire test suite"},
+          t = {"<cmd>TestLast<cr>", "run the last test"},
+          -- e = { "<cmd>OpenGoTestFile<cr>", "open file"},
+          -- v = { "<cmd>OpenGoTestFileVSplit<cr>", "open file in vert split"},
+      }
+    },
+    { prefix = "<leader>" }
+)
