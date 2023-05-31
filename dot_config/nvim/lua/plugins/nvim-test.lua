1a3aa4e59ef01a6b255c088f5bac48d49879cf0c
Siddhanta <siddhanta.c@gmail.com>
2023-05-31 17:48:39 +0800

    [nvim] Remove unnecessary stuff from test command

--- dot_config/nvim/lua/plugins/nvim-test.lua
+++ dot_config/nvim/lua/plugins/nvim-test.lua
@@ -17,7 +17,7 @@ require ('which-key').register(
   {
       t = {
           name = "Test",
-          n = {"<cmd>TestNearest AWS_SECRET_ACCESS_KEY=123 AWS_ACCESS_KEY_ID=123 DH_SPEC_FILE=$(pwd)/deployments/development/spec_local.yaml<cr>", "run test nearest to cursor"},
+          n = {"<cmd>TestNearest<cr>", "run test nearest to cursor"},
           f = {"<cmd>TestFile<cr>", "run test file"},
           s = {"<cmd>TestSuite<cr>", "run entire test suite"},
           t = {"<cmd>TestLast<cr>", "run the last test"},
