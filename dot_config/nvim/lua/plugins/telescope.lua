653f5be8ada5d6f9125db307b6530967d39a95ad
Siddhanta <siddhanta.c@gmail.com>
2023-06-01 11:49:23 +0800

    [nvim] Add ability to delete buffers from telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ require('telescope').setup{
     mappings = {
       i = {
       ["<esc>"] = require('telescope.actions').close,
+      ['<c-d>'] = require('telescope.actions').delete_buffer,
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
