d808b2bb043e31757779763d713b80e18edb1e93
Siddhanta <siddhanta.c@gmail.com>
2023-06-18 14:49:50 +0800

    nvim: lazy work contd 3

---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'tpope/vim-fugitive',
+    },
+    'tpope/vim-rhubarb',
+    'tpope/vim-surround',
+    -- 'tpope/vim-unimpaired'
+
+    'rcarriga/nvim-notify',
+    'jiangmiao/auto-pairs',
+    'szw/vim-maximizer',
+    'numToStr/Comment.nvim',
+
+    "onsails/lspkind.nvim", -- vs-code like icons for autocompletion
+}
