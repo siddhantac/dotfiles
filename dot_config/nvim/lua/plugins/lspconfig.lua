fddb9b6bae76ca524b3bc1e5e7d7e9004fb16169
Siddhanta <siddhanta.c@gmail.com>
2023-06-25 10:13:26 +0800

    nvim: update lua lsp settings

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,10 @@
 local spec = {
-        'neovim/nvim-lspconfig', -- Configurations for Nvim LSP
-        name = "lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-        },
+    'neovim/nvim-lspconfig',     -- Configurations for Nvim LSP
+    name = "lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
+    },
 }
 
 function spec:config()
@@ -14,37 +14,37 @@ function spec:config()
     -- lspconfig setup
     -- enable keybinds only for when lsp server available
     -- local on_attach = function(client, bufnr)
-      -- keybind options
-      -- local opts = { noremap = true, silent = true, buffer = bufnr }
+    -- keybind options
+    -- local opts = { noremap = true, silent = true, buffer = bufnr }
 
-      require ('which-key').register(
+    require('which-key').register(
         {
             l = {
                 name = "LSP",
                 -- s = {vim.lsp.buf.signature_help, "Signature help"},
                 -- n = {vim.lsp.buf.rename, "Rename"},
-                i = {vim.lsp.buf.implementation, "Implementation"},
-                I = {"<cmd>Telescope lsp_implementations<cr>", "Implementation"},
-                h = {vim.lsp.buf.hover, "Hover"},
-                y = {vim.lsp.buf.type_definition, "Go to type def"},
-                x = {vim.lsp.buf.format, "Format"},
-                D = {vim.lsp.buf.definition, "Go to def"},
-                r = {vim.lsp.buf.references, "References in loc list"},
-                R = {'<cmd>Telescope lsp_references<CR>', "References in Telescope"},
-                s = {'<cmd>Telescope lsp_document_symbols<CR>', "Document symbols"},
-                S = {'<cmd>Telescope lsp_workspace_symbols<CR>', "Workspace symbols"},
-                l = {'<cmd>LspInfo<CR>', "LSP Info"},
+                i = { vim.lsp.buf.implementation, "Implementation" },
+                I = { "<cmd>Telescope lsp_implementations<cr>", "Implementation" },
+                h = { vim.lsp.buf.hover, "Hover" },
+                y = { vim.lsp.buf.type_definition, "Go to type def" },
+                x = { vim.lsp.buf.format, "Format" },
+                D = { vim.lsp.buf.definition, "Go to def" },
+                r = { vim.lsp.buf.references, "References in loc list" },
+                R = { '<cmd>Telescope lsp_references<CR>', "References in Telescope" },
+                s = { '<cmd>Telescope lsp_document_symbols<CR>', "Document symbols" },
+                S = { '<cmd>Telescope lsp_workspace_symbols<CR>', "Workspace symbols" },
+                l = { '<cmd>LspInfo<CR>', "LSP Info" },
 
 
-                d = {"<cmd>Lspsaga peek_definition<CR>", "Peek definition"},
-                n = {"<cmd>Lspsaga rename<CR>", "Rename"},
-                f = {"<cmd>Lspsaga lsp_finder<CR>", "Finder"},
-                a = {"<cmd>Lspsaga code_action<CR>", "Code action"},
-                o = {"<cmd>Lspsaga outline<CR>", "Show outline"},
+                d = { "<cmd>Lspsaga peek_definition<CR>", "Peek definition" },
+                n = { "<cmd>Lspsaga rename<CR>", "Rename" },
+                f = { "<cmd>Lspsaga lsp_finder<CR>", "Finder" },
+                a = { "<cmd>Lspsaga code_action<CR>", "Code action" },
+                o = { "<cmd>Lspsaga outline<CR>", "Show outline" },
             }
-          },
-          { prefix = "<leader>" }
-      )
+        },
+        { prefix = "<leader>" }
+    )
     -- end
 
     -- local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -53,8 +53,8 @@ function spec:config()
     -- (not in youtube nvim video)
     local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
     for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
     lspconfig.gopls.setup {
@@ -64,39 +64,41 @@ function spec:config()
 
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      -- on_attach = on_attach,
-      settings = { -- custom settings for lua
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
+        -- capabilities = capabilities,
+        -- on_attach = on_attach,
+        settings = { -- custom settings for lua
+            Lua = {
+                -- make the language server recognize "vim" global
+                diagnostics = {
+                    globals = { "vim" },
+                },
+                telemetry = {
+                    enable = false,
+                },
+                --     workspace = {
+                --       -- make language server aware of runtime files
+                --       library = {
+                --         [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                --         [vim.fn.stdpath("config") .. "/lua"] = true,
+                --       },
+                --     },
             },
-          },
         },
-      },
     })
 
     lspconfig.yamlls.setup {}
     lspconfig.pyright.setup {}
     lspconfig.tsserver.setup {}
     lspconfig.jsonls.setup {
-      capabilities = capabilities,
-      commands = {
-        Format = {
-          function()
-            vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
-          end
+        capabilities = capabilities,
+        commands = {
+            Format = {
+                function()
+                    vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+                end
+            }
         }
-      }
-}
+    }
 end
 
 return spec
-
