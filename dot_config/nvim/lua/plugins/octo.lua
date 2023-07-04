dafb248bd16c533b1cf93f87e2782a4886863136
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2023-07-04 14:15:27 +0800

    nvim: remap keys to submit PR review (#4)

--- dot_config/nvim/lua/plugins/octo.lua
+++ dot_config/nvim/lua/plugins/octo.lua
@@ -1,12 +1,21 @@
 return {
-  'pwntester/octo.nvim',
-  requires = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    'nvim-tree/nvim-web-devicons',
-  },
-    cmd = {"Octo"},
-  config = function ()
-    require"octo".setup()
-  end,
+    'pwntester/octo.nvim',
+    requires = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+    cmd = { "Octo" },
+    config = function()
+        require "octo".setup({
+            mappings = {
+                submit_win = {
+                    approve_review = { lhs = "<leader>a", desc = "approve review" },
+                    comment_review = { lhs = "<leader>m", desc = "comment review" },
+                    request_changes = { lhs = "<leader>r", desc = "request changes review" },
+                    close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
+                },
+            }
+        })
+    end,
 }
