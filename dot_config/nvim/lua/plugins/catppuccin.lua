570d989d582ea999a30efb1f89abe73823bcd4c6
Siddhanta <siddhanta.c@gmail.com>
2023-07-06 14:30:09 +0800

    nvim: simplify catpuccin config (also fixes no color in nvim-notify)

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,17 +1,4 @@
-local spec = {
-  "catppuccin/nvim",
-  name = "catppuccin.nvim",
-  priority = 10000,
+return {
+    "catppuccin/nvim",
+    priority = 10000,
 }
-
-function spec:config()
-  local catppuccin = require("catppuccin")
-
-  catppuccin.setup({
-    flavour = "mocha",
-  })
-
-  catppuccin.load()
-end
-
-return spec
