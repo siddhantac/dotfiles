645998454c6d23bdac500fe3a20e5bf2dc989994
Siddhanta <siddhanta.c@gmail.com>
2023-07-19 12:10:52 +0800

    updated telescope according to astronvim settings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,65 +7,37 @@ local spec = {
 }
 
 function spec:config()
+    local actions = require "telescope.actions"
+
     require('telescope').setup {
         defaults = {
-            -- Default configuration for telescope goes here:
-            -- config_key = value,layout_config =
-            -- other layout configuration here
-            -- path_display = "shorten",
+            path_display = { "truncate" },
+            sorting_strategy = "ascending",
+            layout_config = {
+                horizontal = { prompt_position = "top", preview_width = 0.55 },
+                vertical = { mirror = false },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120,
+            },
             mappings = {
                 i = {
-                    ["<esc>"] = require('telescope.actions').close,
+                    ["<C-n>"] = actions.cycle_history_next,
+                    ["<C-p>"] = actions.cycle_history_prev,
+                    ["<C-j>"] = actions.move_selection_next,
+                    ["<C-k>"] = actions.move_selection_previous,
                     ['<c-d>'] = require('telescope.actions').delete_buffer,
-                    -- map actions.which_key to <C-h> (default: <C-/>)
-                    -- actions.which_key shows the mappings for your picker,
-                    -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                    -- ["<C-h>"] = "which_key"
+                    -- ["<esc>"] = require('telescope.actions').close, -- replaced this with 'q' in normal mode
                 },
-            },
-        },
-        pickers = {
-            -- Default configuration for builtin pickers goes here:
-            -- picker_name = {
-            --   picker_config_key = value,
-            --   ...
-            -- }
-            -- Now the picker_config_key will be applied every time you call this
-            -- builtin picker
-            lsp_document_symbols = {
-                theme = "dropdown",
-            },
-            lsp_references = {
-                theme = "dropdown",
-                layout_config = {
-                    -- increase overall width
-                    width = function(_, max_columns)
-                        local percentage = 0.75
-                        local max = 150
-                        return math.min(math.floor(percentage * max_columns), max)
-                    end
-                },
-                -- to view long filenames I tried to hide the column on the right
-                -- side of filenames
-                include_declaration = false, -- doesn't work 
-                fname_width = 100,           -- allows long filename to be read
-            },
-            find_files = {
-                theme = "dropdown",
-                hidden = true,
+                n = { q = actions.close },
             },
         },
         extensions = {
-            -- Your extension configuration goes here:
-            -- extension_name = {
-            --   extension_config_key = value,
-            -- }
-            -- please take a look at the readme of the extension you want to configure
             fzf = {
-                fuzzy = true,               -- false will only do exact matching
+                fuzzy = true,                   -- false will only do exact matching
                 override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case",   -- or "ignore_case" or "respect_case"
+                override_file_sorter = true,    -- override the file sorter
+                case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
                 -- the default case_mode is "smart_case"
             }
         }
