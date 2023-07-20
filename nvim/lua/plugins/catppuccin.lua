51c24a6fd44e49d40c1ffc4a8aee9ecac8f73267
Siddhanta <siddhanta.c@gmail.com>
2023-07-20 16:07:04 +0800

    integrate catppuccin with other plugins

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,4 +1,45 @@
+local telescopeBorderless = function(flavor)
+    local cp = require("catppuccin.palettes").get_palette(flavor)
+
+    return {
+        TelescopeMatching = { fg = cp.flamingo },
+        TelescopeSelection = { fg = cp.text, bg = cp.surface0, bold = true },
+
+        TelescopePromptPrefix = { bg = cp.surface0 },
+        TelescopePromptNormal = { bg = cp.surface0 },
+        TelescopeResultsNormal = { bg = cp.mantle },
+        TelescopePreviewNormal = { bg = cp.mantle },
+        TelescopePromptBorder = { bg = cp.surface0, fg = cp.surface0 },
+        TelescopeResultsBorder = { bg = cp.mantle, fg = cp.mantle },
+        TelescopePreviewBorder = { bg = cp.mantle, fg = cp.mantle },
+        TelescopePromptTitle = { bg = cp.pink, fg = cp.mantle },
+        TelescopeResultsTitle = { fg = cp.mantle },
+        TelescopePreviewTitle = { bg = cp.green, fg = cp.mantle },
+    }
+end
+
 return {
     "catppuccin/nvim",
-    priority = 10000,
+    priority = 1,
+    config = function()
+        require("catppuccin").setup({
+            integrations = {
+                telescope = { enabled = true },
+                harpoon = true,
+                lsp_saga = true,
+                mini = true,
+                neotest = true,
+                notify = true,
+                nvimtree = true,
+                which_key = true,
+                -- gitsigns = true,
+            },
+            highlight_overrides = {
+                latte = telescopeBorderless('latte'),
+                frappe = telescopeBorderless('frappe'),
+                macchiato = telescopeBorderless('macchiato'),
+                mocha = telescopeBorderless('mocha'),
+            }
+        })
+    end
 }
