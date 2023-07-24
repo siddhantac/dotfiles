12eb5ba079a70c3dee14f0a86c6f77eac3275bb5
Siddhanta <siddhanta.c@gmail.com>
2023-07-24 16:13:09 +0800

    improve resession behaviour

--- nvim/lua/plugins/resession.lua
+++ nvim/lua/plugins/resession.lua
@@ -1,5 +1,5 @@
 local function get_session_name()
-    return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    return vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- returns directory name
 end
 
 local spec = {
@@ -16,15 +16,15 @@ local spec = {
 
         local resession = require("resession")
         -- autoload session
-        -- vim.api.nvim_create_autocmd("VimEnter", {
-        --   callback = function()
-        --     -- Only load the session if nvim was started with no args
-        --     if vim.fn.argc(-1) == 0 then
-        --     -- Save these to a different directory, so our manual sessions don't get polluted
-        --       resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
-        --     end
-        --   end,
-        -- })
+        vim.api.nvim_create_autocmd("VimEnter", {
+          callback = function()
+            -- Only load the session if nvim was started with no args
+            if vim.fn.argc(-1) == 0 then
+            -- Save these to a different directory, so our manual sessions don't get polluted
+              resession.load(get_session_name(), { dir = "session", silence_errors = true })
+            end
+          end,
+        })
         vim.api.nvim_create_autocmd("VimLeavePre", {
             callback = function()
                 resession.save(get_session_name(), { dir = "session", notify = false })
