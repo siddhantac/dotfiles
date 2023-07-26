6b25412148736e6486bc44efe1c2098ce6996ff1
Siddhanta <siddhanta.c@gmail.com>
2023-07-26 13:31:05 +0800

    formatting

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -6,23 +6,41 @@ return {
     {
         'numToStr/Comment.nvim',
         event = { "BufRead" },
-        config = function()
-            require('Comment').setup()
-        end,
+        opts = {},
     },
     {
         'stevearc/aerial.nvim',
-        opts = {},
+        opts = {
+            attach_mode = "global",
+            backends = { "lsp", "treesitter", "markdown", "man" },
+            -- disable_max_lines = vim.g.max_file.lines,
+            -- disable_max_size = vim.g.max_file.size,
+            layout = { min_width = 28 },
+            show_guides = true,
+            filter_kind = false,
+            guides = {
+                mid_item = "├ ",
+                last_item = "└ ",
+                nested_top = "│ ",
+                whitespace = "  ",
+            },
+            keymaps = {
+                ["[y"] = "actions.prev",
+                ["]y"] = "actions.next",
+                ["[Y"] = "actions.prev_up",
+                ["]Y"] = "actions.next_up",
+                ["{"] = false,
+                ["}"] = false,
+                ["[["] = false,
+                ["]]"] = false,
+            },
+        },
+
         -- Optional dependencies
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons"
         },
-        config = function()
-            require("aerial").setup({
-                backends = { "treesitter", "lsp", "markdown", "man" },
-            })
-        end
     },
 
     {
