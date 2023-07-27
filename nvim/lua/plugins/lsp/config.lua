fba82affce5c6df06c217a67440722f19dfa551c
Siddhanta <siddhanta.c@gmail.com>
2023-07-27 13:52:29 +0800

    moved lsp keybinds to which-key because they were not loading otherwise

--- nvim/lua/plugins/lsp/config.lua
+++ nvim/lua/plugins/lsp/config.lua
@@ -1,30 +1,4 @@
 return function(client, bufnr)
-    require('which-key').register({
-        ["gd"] = { vim.lsp.buf.goto_definition(), "Go to def"},
-        ["<leader>lf"] = { vim.lsp.buf.format, "Format" },
-        ["<leader>lh"] = { vim.lsp.buf.hover, "Hover" },
-        ["<leader>lm"] = { "<cmd>Telescope lsp_implementations<cr>", "Implementation" },
-        ["<leader>lr"] = { '<cmd>Telescope lsp_references<CR>', "Refs in Telescope" },
-        ["<leader>ls"] = {
-            function()
-                local aerial_avail, _ = pcall(require, "aerial")
-                if aerial_avail then
-                    require("telescope").extensions.aerial.aerial()
-                else
-                    require("telescope.builtin").lsp_document_symbols()
-                end
-            end, "Document symbols" },
-        ["<leader>ld"] = { "<cmd>Lspsaga peek_definition<CR>", "Peek def" },
-        ["<leader>ly"] = { "<cmd>Lspsaga peek_type_definition<CR>", "Peek type def" },
-        ["<leader>ln"] = { "<cmd>Lspsaga rename<CR>", "Rename" },
-        ["<leader>lx"] = { "<cmd>Lspsaga finder<CR>", "Finder" },
-        ["<leader>la"] = { "<cmd>Lspsaga code_action<CR>", "Code action" },
-        ["<leader>li"] = { "<cmd>Lspsaga incoming_calls<CR>", "Incoming calls" },
-        ["<leader>lu"] = { "<cmd>Lspsaga outgoing_calls<CR>", "Outgoing calls" },
-        ["<leader>lo"] = { "<cmd>AerialToggle<CR>", "Show outline" },
-        -- S = { '<cmd>Telescope lsp_workspace_symbols<CR>', "Workspace symbols" },
-    })
-
     -- Only highlight if compatible with the language
     if client.server_capabilities.documentHighlightProvider then
         local lsp_highlight = vim.api.nvim_create_augroup("LspHighlight", {clear = true})
@@ -51,12 +25,12 @@ return function(client, bufnr)
         })
     end
 
-    if client.server_capabilities.documentFormattingProvider then
+    -- if client.server_capabilities.documentFormattingProvider then
         vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = bufnr,
             callback = function()
                 vim.lsp.buf.format()
             end,
         })
-    end
+    -- end
 end
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -161,6 +161,29 @@ function spec:config()
         -- LSP
         ["<leader>l"] = { name = "LSP", desc = get_icon("ActiveLSP") .. "LSP" },
         ["<leader>ll"] = {"<cmd>LspInfo<CR>", "LSP Info"},
+        ["gd"] = { vim.lsp.buf.goto_definition, "Go to def"},
+        ["<leader>lf"] = { vim.lsp.buf.format, "Format" },
+        ["<leader>lh"] = { vim.lsp.buf.hover, "Hover" },
+        ["<leader>lm"] = { "<cmd>Telescope lsp_implementations<cr>", "Implementation" },
+        ["<leader>lr"] = { '<cmd>Telescope lsp_references<CR>', "Refs in Telescope" },
+        ["<leader>ls"] = {
+            function()
+                local aerial_avail, _ = pcall(require, "aerial")
+                if aerial_avail then
+                    require("telescope").extensions.aerial.aerial()
+                else
+                    require("telescope.builtin").lsp_document_symbols()
+                end
+            end, "Document symbols" },
+        ["<leader>ld"] = { "<cmd>Lspsaga peek_definition<CR>", "Peek def" },
+        ["<leader>ly"] = { "<cmd>Lspsaga peek_type_definition<CR>", "Peek type def" },
+        ["<leader>ln"] = { "<cmd>Lspsaga rename<CR>", "Rename" },
+        ["<leader>lx"] = { "<cmd>Lspsaga finder<CR>", "Finder" },
+        ["<leader>la"] = { "<cmd>Lspsaga code_action<CR>", "Code action" },
+        ["<leader>li"] = { "<cmd>Lspsaga incoming_calls<CR>", "Incoming calls" },
+        ["<leader>lu"] = { "<cmd>Lspsaga outgoing_calls<CR>", "Outgoing calls" },
+        ["<leader>lo"] = { "<cmd>AerialToggle<CR>", "Show outline" },
+        -- S = { '<cmd>Telescope lsp_workspace_symbols<CR>', "Workspace symbols" },
     })
 
     whichkey.register(
