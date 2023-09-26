8d1486c5608f827c06011ecedabe1ecf6d7f58ad
Siddhanta <siddhanta.c@gmail.com>
2023-09-26 12:04:51 +0800

    nvim: add neogit plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,15 @@
 return {
     {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "nvim-telescope/telescope.nvim", -- optional
+            "sindrets/diffview.nvim", -- optional
+            "ibhagwan/fzf-lua",      -- optional
+        },
+        config = true
+    },
+    {
         -- works better than tpope/vim-rhubarb
         'ruifm/gitlinker.nvim',
         dependencies = 'nvim-lua/plenary.nvim',
