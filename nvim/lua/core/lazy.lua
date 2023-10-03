6f5a2c81e253c2666285706b758a4645716afe21
Siddhanta <siddhanta.c@gmail.com>
2023-10-03 15:25:49 +0800

    refactor(nvim): moved lazy installation etc into init.lua

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,34 +1,21 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 local lazy = require("lazy")
 
 lazy.setup("plugins", {
     performance = {
         rtp = {
-          disabled_plugins = {
-            "netrwPlugin",
-            "gzip",
-            "health",
-            "man",
-            "matchit",
-            "matchparen",
-            "rplugin",
-            "tarPlugin",
-            "tohtml",
-            "tutor",
-            "zipPlugin",
-          },
+            disabled_plugins = {
+                "netrwPlugin",
+                "gzip",
+                "health",
+                "man",
+                "matchit",
+                "matchparen",
+                "rplugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
         },
-  },
+    },
 })
