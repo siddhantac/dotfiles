3a1759a29ad9cba1a1808308a76bdd6bc1652584
Siddhanta <siddhanta.c@gmail.com>
2023-10-03 16:54:21 +0800

    Moved git plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,4 +21,59 @@ require("lazy").setup({
     },
 
     { 'christoomey/vim-tmux-navigator' },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function
+    },
+
+    -- Git plugins
+    {
+        'ldelossa/gh.nvim',
+        dependencies = { 'ldelossa/litee.nvim' },
+        config = function()
+            require('litee.lib').setup()
+            require('litee.gh').setup()
+        end,
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "nvim-telescope/telescope.nvim", -- optional
+            "sindrets/diffview.nvim",        -- optional
+            "ibhagwan/fzf-lua",              -- optional
+        },
+        config = true
+    },
+    {
+        -- works better than tpope/vim-rhubarb
+        'ruifm/gitlinker.nvim',
+        dependencies = 'nvim-lua/plenary.nvim',
+        opts = {},
+    },
+    { 'tpope/vim-fugitive' },
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require("gitsigns").setup()
+        end
+    },
+    {
+        'pwntester/octo.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        cmd = { "Octo" },
+        config = function()
+            require "plugins.octo".setup()
+        end,
+    },
 })
