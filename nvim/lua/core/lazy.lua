51d7363ea11718c65228b60dfd7f1c521512cbd6
Siddhanta <siddhanta.c@gmail.com>
2023-11-15 18:04:07 +0800

    feat(nvim): lazy will check for updates automatically

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,6 +1,16 @@
 local lazy = require("lazy")
 
 lazy.setup("plugins", {
+    checker = {
+        -- automatically check for plugin updates
+        enabled = true,
+        notify = true, -- get a notification when new updates are found
+    },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = true,
+        notify = false, -- get a notification when changes are found
+    },
     performance = {
         rtp = {
             disabled_plugins = {
