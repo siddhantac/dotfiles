74366bb35e2b6c1e385090bbc0051df2fda25847
Siddhanta <siddhanta.c@gmail.com>
2023-11-20 10:49:53 +0800

    feat(nvim): fix keybinds to navigate completion menu

--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -12,7 +12,9 @@ M.setup = function()
             end,
         },
         mapping = cmp.mapping.preset.insert({
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-k>'] = cmp.mapping.select_prev_item(),
+            ['<C-j>'] = cmp.mapping.select_next_item(),
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<CR>'] = cmp.mapping.confirm {
@@ -41,6 +43,8 @@ M.setup = function()
         sources = {
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
+            { name = 'buffer' },
+            { name = 'path' },
         },
     }
 
