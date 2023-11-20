4b44d0fe50bf4de5a1129e00e73b3efe8d5d2119
Siddhanta <siddhanta.c@gmail.com>
2023-11-20 18:52:49 +0800

    add icons to mason

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -4,6 +4,17 @@ M.setup = function()
     mason.setup() -- enable mason
 
     local mason_lspconfig = require("mason-lspconfig")
+
+    mason.setup({
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+    })
+
     mason_lspconfig.setup({
         -- list of servers for mason to install
         ensure_installed = {
