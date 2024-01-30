58e89be7fbd2d3f99300620bacc5b225473edfd6
Siddhanta <siddhanta.c@gmail.com>
2024-01-30 10:50:28 +0800

    nvim: add jumplist to quickfix

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -294,4 +294,23 @@ function M.harpoon()
     nmap({ "<leader>h[", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", { desc = "Prev" } })
 end
 
+function M.jumplist()
+    local jumplist, _ = unpack(vim.fn.getjumplist())
+    local qf_list = {}
+    for _, v in pairs(jumplist) do
+        if vim.fn.bufloaded(v.bufnr) == 1 then
+            table.insert(qf_list, {
+                bufnr = v.bufnr,
+                lnum = v.lnum,
+                col = v.col,
+                text = vim.api.nvim_buf_get_lines(v.bufnr, v.lnum - 1, v.lnum, false)[1],
+            })
+        end
+    end
+    vim.fn.setqflist(qf_list, " ")
+    vim.cmd("copen")
+end
+
+nmap({ "<leader>j", M.jumplist, { desc = "Jumplist" } })
+
 return M
