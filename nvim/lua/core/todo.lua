917f6a2a687fdb93bfe161eefd97d92e8e776d45
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-03-05 23:16:00 +0530

    project & ctx regex working

--- nvim/lua/core/todo.lua
+++ nvim/lua/core/todo.lua
@@ -3,16 +3,16 @@ augroup TodotxtSyntax
     autocmd!
     autocmd BufReadPost *.todotxt set filetype=todotxt
 
-    autocmd BufReadPost * syntax match TodoPriorityA /^(A)/
+    autocmd BufReadPost * syntax match TodoPriorityA /^(A)/ contains=Context,Project
     autocmd BufReadPost * syntax match TodoPriorityB /^(B)/
     autocmd BufReadPost * syntax match TodoPriorityC /^(C)/
-    autocmd BufReadPost * syntax match Context /@.*\s/
-    autocmd BufReadPost * syntax match Project /+.*\s/
+    autocmd BufReadPost * syntax match Context /@\w*/ contains=NONE
+    autocmd BufReadPost * syntax match Project /+\w*/ contains=NONE
 
     autocmd BufReadPost * highlight TodoPriorityA  ctermfg=red guifg=red
     autocmd BufReadPost * highlight TodoPriorityB  ctermfg=cyan guifg=cyan
     autocmd BufReadPost * highlight TodoPriorityC  ctermfg=yellow guifg=yellow
-    autocmd BufReadPost * highlight Context  ctermfg=blue guifg=blue
+    autocmd BufReadPost * highlight Context  ctermfg=yellow guifg=yellow
     autocmd BufReadPost * highlight Project  ctermfg=green guifg=green
 augroup END
 ]])
