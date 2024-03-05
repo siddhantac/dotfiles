c2e3fa4a2e305937b7940d6b9b24490d6c12b35a
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-03-05 23:17:28 +0530

    better names for highlight groups

--- nvim/lua/core/todo.lua
+++ nvim/lua/core/todo.lua
@@ -6,14 +6,14 @@ augroup TodotxtSyntax
     autocmd BufReadPost * syntax match TodoPriorityA /^(A)/
     autocmd BufReadPost * syntax match TodoPriorityB /^(B)/
     autocmd BufReadPost * syntax match TodoPriorityC /^(C)/
-    autocmd BufReadPost * syntax match Context /@\w*/
-    autocmd BufReadPost * syntax match Project /+\w*/
+    autocmd BufReadPost * syntax match TodoContext /@\w*/
+    autocmd BufReadPost * syntax match TodoProject /+\w*/
 
     autocmd BufReadPost * highlight TodoPriorityA  ctermfg=red guifg=red
     autocmd BufReadPost * highlight TodoPriorityB  ctermfg=cyan guifg=cyan
     autocmd BufReadPost * highlight TodoPriorityC  ctermfg=yellow guifg=yellow
-    autocmd BufReadPost * highlight Context  ctermfg=yellow guifg=yellow
-    autocmd BufReadPost * highlight Project  ctermfg=green guifg=green
+    autocmd BufReadPost * highlight TodoContext  ctermfg=yellow guifg=yellow
+    autocmd BufReadPost * highlight TodoProject  ctermfg=green guifg=green
 augroup END
 ]])
 -- vim.api.nvim_set_hl(0, "Kalu", { ctermfg = red, fg = red })
