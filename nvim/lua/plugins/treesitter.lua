a587f03d27c240c29b415f8e73f94ad326839424
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-03-07 23:53:59 +0530

    nvim: add bash treesitter parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ M.config = function()
     require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all"
         ensure_installed = {
+            "bash",
             "csv",
             "go",
             "gomod",
