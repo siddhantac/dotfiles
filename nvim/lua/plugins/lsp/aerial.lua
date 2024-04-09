8c49d7b40b7099c705bd709bb437ddf35b05c10a
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-04-09 16:12:28 +0800

    nvim: remove unused keymaps for aerial

--- nvim/lua/plugins/lsp/aerial.lua
+++ nvim/lua/plugins/lsp/aerial.lua
@@ -13,15 +13,5 @@ M.opts = {
         nested_top = "│ ",
         whitespace = "  ",
     },
-    keymaps = {
-        ["[y"] = "actions.prev",
-        ["]y"] = "actions.next",
-        ["[Y"] = "actions.prev_up",
-        ["]Y"] = "actions.next_up",
-        ["{"] = false,
-        ["}"] = false,
-        ["[["] = false,
-        ["]]"] = false,
-    },
 }
 return M
