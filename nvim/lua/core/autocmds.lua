e8f7ad1f1cbaef914bc46107786bad721fe1076c
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-05-15 16:15:59 +0800

    formatting

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -11,8 +11,8 @@ local api = vim.api
 -- Highlight on yank
 local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
 api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+    command = "silent! lua vim.highlight.on_yank()",
+    group = yankGrp,
 })
 
 -- Autosave only when there is something to save,
@@ -23,7 +23,7 @@ api.nvim_create_autocmd(
 )
 
 -- Show relative numbers only for the active buffer
---  don't run the autocmd for telescopeprompt, 
+--  don't run the autocmd for telescopeprompt,
 --  prevents the annoying 0 from showing next to the cursor in the prompt
 local ignoreFiletype = { 'TelescopePrompt' }
 local linenumtoggle = api.nvim_create_augroup("LineNumberToggle", { clear = true })
@@ -48,7 +48,7 @@ api.nvim_create_autocmd("BufEnter",
 )
 
 -- Organise imports
---     works, but the cursor flickers and there are 
+--     works, but the cursor flickers and there are
 --     many error notifications that 'code action is not available'
 --     see the stuff below instead.
 -- vim.api.nvim_create_autocmd('BufWritePre', {
@@ -69,34 +69,34 @@ api.nvim_create_autocmd("BufEnter",
 --   https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
 local function goimports(wait_ms)
     local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
+    params.context = { only = { "source.organizeImports" } }
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
     for _, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-        else
-          vim.lsp.buf.execute_command(r.command)
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
         end
-      end
     end
-  end
+end
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
+    pattern = '*.go',
+    callback = function()
         goimports(1000)
-  end
+    end
 })
 
--- save a session before exiting 
+-- save a session before exiting
 -- note: even though mini.sessions has a feature to autowrite a session
---        on exit, it was hard to come up with a way to automatically 'create' 
---        a session in the first place. The below takes care of everything 
+--        on exit, it was hard to come up with a way to automatically 'create'
+--        a session in the first place. The below takes care of everything
 --        in 6 lines.
 vim.api.nvim_create_autocmd('VimLeavePre', {
     pattern = '*',
-    callback = function ()
+    callback = function()
         local dirname = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- returns directory name
         require('mini.sessions').write(dirname)
     end
