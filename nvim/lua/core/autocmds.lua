1a7b3a76e421f4435d4a0f8bb351a8ad0d9dfcad
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-05-15 16:16:29 +0800

    experiment: save on insert-leave

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -21,6 +21,10 @@ api.nvim_create_autocmd(
     "FocusLost",
     { pattern = "*", command = "silent! wa" }
 )
+api.nvim_create_autocmd(
+    "InsertLeave",
+    { pattern = "*", command = "silent! wa" }
+)
 
 -- Show relative numbers only for the active buffer
 --  don't run the autocmd for telescopeprompt,
