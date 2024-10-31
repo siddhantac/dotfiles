639c704792492485e9715082a9d62365c31ff5d9
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-10-31 12:55:10 +0800

    pr_title script will take the URL from clipboard and output markdown compatible link

--- scripts/get_pr_title.sh
+++ scripts/get_pr_title.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+link=$(pbpaste)
 token=$(grep -o 'GITHUB_TOKEN.*' ~/.aliases.local | sed -E "s/.*=(.*)/\\1/")
-echo $(GITHUB_TOKEN=${token} gh pr view $1 --json author,title  --jq  '"\(.author.login) | \(.title)"' )
+display=$(echo $link | rg "deliveryhero/(.*)/pull" -or '$1')
+echo $(GITHUB_TOKEN=${token} gh pr view $link --json author,title  --jq  '"\(.author.login) | \(.title)"' ) \[$display\]\($link\)
