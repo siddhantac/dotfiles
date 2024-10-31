d2abf9a7dbceae491cbd37a30c6e99ca64d110e8
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-10-31 12:11:39 +0800

    Update Github pr script to work with PR url instead of number
    This allows me to use it for PRs across all repos

--- scripts/get_pr_title.sh
+++ scripts/get_pr_title.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 token=$(grep -o 'GITHUB_TOKEN.*' ~/.aliases.local | sed -E "s/.*=(.*)/\\1/")
-echo $(GITHUB_TOKEN=${token} gh pr view https://github.com/deliveryhero/pd-pablo-payment-gateway/pull/$1 --json author,title  --jq  '"\(.author.login) | \(.title)"' )
+echo $(GITHUB_TOKEN=${token} gh pr view $1 --json author,title  --jq  '"\(.author.login) | \(.title)"' )
