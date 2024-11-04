08a59d3ef661aecd7c806502605b9fd3134b75af
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-11-04 14:21:20 +0800

    nvim: added plugin for PR reviews

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -508,4 +508,37 @@ require("lazy").setup({
         opts = {},
         keys = core_mappings.overseer_mappings,
     },
+    {
+        'daliusd/ghlite.nvim',
+        config = function()
+            require('ghlite').setup({
+                debug = false,   -- if set to true debugging information is written to ~/.ghlite.log file
+                view_split = 'vsplit', -- set to empty string '' to open in active buffer
+                diff_split = 'vsplit', -- set to empty string '' to open in active buffer
+                comment_split = 'split', -- set to empty string '' to open in active buffer
+                open_command = 'open', -- open command to use, e.g. on Linux you might want to use xdg-open
+                merge = {
+                    approved = '--squash',
+                    nonapproved = '--auto --squash',
+                },
+                keymaps = { -- override default keymaps with the ones you prefer
+                    diff = {
+                        open_file = 'gf',
+                        open_file_tab = 'gt',
+                        open_file_split = 'gs',
+                        open_file_vsplit = 'gv',
+                        approve = '<C-A>',
+                    },
+                    comment = {
+                        send_comment = '<C-CR>'
+                    },
+                    pr = {
+                        approve = '<C-A>',
+                        merge = '<C-M>',
+                    },
+                },
+            })
+        end,
+        -- keys = core_mappings.ghlite_mappings,
+    },
 })
