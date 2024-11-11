7d09e54784bfa595de0e96a9b14c40bd8f62d03c
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-11-11 11:35:32 +0800

    Fixed gitconflict autocmd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -139,6 +139,10 @@ vim.api.nvim_create_autocmd('LspProgress', {
 vim.api.nvim_create_autocmd('User', {
     pattern = 'GitConflictDetected',
     callback = function()
-        vim.notify('Conflict detected in ' .. vim.fn.expand('<afile>'))
+        -- vim.notify('Conflict detected in ' .. vim.fn.expand('<afile>'))
+        vim.keymap.set('n', 'cww', function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+        end)
     end
 })
