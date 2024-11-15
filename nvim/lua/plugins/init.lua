ecc931de82af9eb0e47b8e1d771f3fd18422c244
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-11-15 22:58:58 +0800

    removed noice.nvim plugin (was commented)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -328,53 +328,6 @@ require("lazy").setup({
         end
     },
 
-    -- 2024-07-17 disabled noice.nvim
-    --   hides the commandline and also `recording @` messages. They need to be shown by adding new components to lualine.
-    --   see ref: https://github.com/folke/noice.nvim?tab=readme-ov-file#-statusline-components
-    --   adding those things to lualine makes it slow. the `recording` message takes a beat before showing up
-    --
-    -- {
-    --     "folke/noice.nvim",
-    --     event = "VeryLazy",
-    --     opts = {
-    --         -- add any options here
-    --         routes = {
-    --             {
-    --                 filter = {
-    --                     event = "msg_show",
-    --                     kind = "",
-    --                     find = "written",
-    --                 },
-    --                 opts = { skip = true },
-    --             },
-    --         },
-    --         lsp = {
-    --             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    --             override = {
-    --                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-    --                 ["vim.lsp.util.stylize_markdown"] = true,
-    --                 -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    --             },
-    --         },
-    --         -- you can enable a preset for easier configuration
-    --         presets = {
-    --             -- bottom_search = true,         -- use a classic bottom cmdline for search
-    --             -- command_palette = true,       -- position the cmdline and popupmenu together
-    --             long_message_to_split = true, -- long messages will be sent to a split
-    --             inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    --             lsp_doc_border = true,        -- add a border to hover docs and signature help
-    --         },
-    --     },
-    --     dependencies = {
-    --         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    --         "MunifTanjim/nui.nvim",
-    --         -- OPTIONAL:
-    --         --   `nvim-notify` is only needed, if you want to use the notification view.
-    --         --   If not available, we use `mini` as the fallback
-    --         "rcarriga/nvim-notify",
-    --     }
-    -- },
-
     {
         'kevinhwang91/nvim-bqf',
         event = 'BufReadPre',
