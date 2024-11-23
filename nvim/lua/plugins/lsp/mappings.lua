065362a7f24f886301513637d8ab4b7d12485d1b
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-11-23 09:30:54 +0800

    Deleted unused lsp mappings

--- nvim/lua/plugins/lsp/mappings.lua
+++
@@ -1,9 +0,0 @@
-local nmap = function(tbl)
-    vim.keymap.set('n', tbl[1], tbl[2], tbl[3])
-end
-
-nmap({ "<leader>lf", vim.lsp.buf.format, { desc = "Format" } })
-nmap({ "<leader>lh", vim.lsp.buf.hover, { desc = "Hover" } })
-nmap({ "<leader>ll", "<cmd>LspInfo<CR>", { desc = "LSP Info" } })
-nmap({ "<leader>lf", vim.lsp.buf.format, { desc = "Format" } })
-nmap({ "<leader>lR", vim.lsp.buf.references, { desc = "Refs (quickfix)" } })
