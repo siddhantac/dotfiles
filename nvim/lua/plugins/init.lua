240fef9b979370ec263174bb31272876d610aace
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-11-25 14:36:36 +0800

    nvim: disabled catpuccin theme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -228,14 +228,14 @@ require("lazy").setup({
             })
         end
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("plugins.catppuccin").setup()
-        end,
-    },
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     priority = 1000,
+    --     config = function()
+    --         require("plugins.catppuccin").setup()
+    --     end,
+    -- },
     {
         'yorickpeterse/nvim-grey',
     },
