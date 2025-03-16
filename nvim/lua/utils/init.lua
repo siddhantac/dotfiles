4275648e006ed57d07bab54af8454bb0cfa38475
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-03-16 22:10:45 +0800

    nvim: use native vim api to call git commands

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -18,46 +18,46 @@ function M.gitpush()
     local title = "Git"
     spinner.start(1, "Pushing", title)
 
-    -- taken from here
-    -- https://www.reddit.com/r/neovim/comments/pa4yle/comment/ha2h1nh/?utm_source=share&utm_medium=web2x&context=3
-    local job = require('plenary.job')
-    job:new({
-        command = 'git',
-        args = { 'push' },
-        on_exit = function(j, exit_code)
-            local res1 = table.concat(j:result(), ":")
-            local res2 = table.concat(j:stderr_result(), ":")
-            local res = res1 .. " " .. res2
+    local cmd = { "git", "push" }
+    vim.system(cmd, { text = true },
+        function(obj)
+            vim.schedule(
+                function()
+                    local res = obj.stdout .. " " .. obj.stderr
 
-            if exit_code ~= 0 then
-                spinner.stop(1, "Failed to push", title)
-                vim.notify(res, "error", { title = title })
-            else
-                spinner.stop(1, "Pushed:" .. res, title)
-            end
-        end,
-    }):start()
+                    if obj.code ~= 0 then
+                        spinner.stop(1, "Failed to push", title)
+                        vim.notify(res, "error", { title = title })
+                    else
+                        spinner.stop(1, "Pushed:" .. res, title)
+                    end
+                end
+            )
+        end
+    )
 end
 
 function M.gitpull()
+    local title = "Git"
     local spinner = require("utils.spinner")
     spinner.start(1, "Pulling", "Git")
 
-    local job = require('plenary.job')
-    job:new({
-        command = 'git',
-        args = { 'pull' },
-        on_exit = function(j, exit_code)
-            local res = table.concat(j:result(), "\n")
+    vim.system(cmd, { text = true },
+        function(obj)
+            vim.schedule(
+                function()
+                    local res = obj.stdout .. " " .. obj.stderr
 
-            if exit_code ~= 0 then
-                spinner.stop(1, "Failed to pull")
-                vim.notify(res, "error", { title = "Git" })
-            else
-                spinner.stop(1, "Pulled")
-            end
-        end,
-    }):start()
+                    if obj.code ~= 0 then
+                        spinner.stop(1, "Failed to pull", title)
+                        vim.notify(res, "error", { title = title })
+                    else
+                        spinner.stop(1, "Pulled:" .. res, title)
+                    end
+                end
+            )
+        end
+    )
 end
 
 return M
