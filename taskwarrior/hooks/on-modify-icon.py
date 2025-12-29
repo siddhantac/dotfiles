ecc0b02c5808d435ccf4040da489dc0b3ccf5a3e
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-12-29 14:16:14 +0800

    taskwarrior: fix hook script

--- taskwarrior/hooks/on-modify-icon.py
+++ taskwarrior/hooks/on-modify-icon.py
@@ -6,13 +6,14 @@ import json
 lines = sys.stdin.readlines()
 original_task = json.loads(lines[0])
 modified_task = json.loads(lines[1])
-prio = modified_task.get("priority", "")
-if prio == "H":
+oldPrio = original_task.get("priority", "")
+newPrio = modified_task.get("priority", "")
+if newPrio == "H" and oldPrio != "H":
     modified_task["icon"] = ""
-    print(f'added icon to \'{modified_task["descrtipion"]}\'')
-else:
+    print(f'added icon to \'{modified_task["description"]}\'')
+elif newPrio != "H" and oldPrio == "H":
     modified_task["icon"] = ""
-    print(f'removed icon from \'{modified_task["descrtipion"]}\'')
+    print(f'removed icon from \'{modified_task["description"]}\'')
 result = json.dumps(modified_task)
 print(result)
 sys.exit(0)
